370e8ef67cd1663b84cfce23b792e3d4a3e28d52
AndreaSdr70 <andrea@sdringola.it>
2024-07-22 15:30:05 +0200

    Selfwork funzioni 3 - Correzione definitiva

---
+++ main.js
@@ -0,0 +1,14 @@
+function contNumber(num) {
+    if(num < 10){
+        return`1 cifra`
+    }else if(num <100){
+        return `2 cifre`
+    }else if(num < 1000){
+        return `3 cifre`
+    }else if(num < 10000){
+        return `4 cifre`
+    }else {
+        return `numero troppo grande`
+    }    
+}
+console.log(contNumber(150));
